80f5d9c2207a4c2a8b7ea3ed065f9a08785d1872
Nate Davis <orpheusdevelopment@gmail.com>
2024-02-08 16:51:43 -0600

    project setup and running, slight tweaks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './index.css';
-import { ReactComponent as BellIcon } from './icons/bell.svg';
-import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
-import { ReactComponent as CaretIcon } from './icons/caret.svg';
-import { ReactComponent as PlusIcon } from './icons/plus.svg';
-import { ReactComponent as CogIcon } from './icons/cog.svg';
-import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
-import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
-import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+import "./index.css";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
+import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
 
-import React, { useState, useEffect, useRef } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import React, { useState, useEffect, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
   return (
     <Navbar>
-      <NavItem icon={<PlusIcon />} />
+      {/* <NavItem icon={<PlusIcon />} />
       <NavItem icon={<BellIcon />} />
-      <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<MessengerIcon />} /> */}
 
       <NavItem icon={<CaretIcon />}>
         <DropdownMenu></DropdownMenu>
@@ -48,13 +48,13 @@ function NavItem(props) {
 }
 
 function DropdownMenu() {
-  const [activeMenu, setActiveMenu] = useState('main');
+  const [activeMenu, setActiveMenu] = useState("main");
   const [menuHeight, setMenuHeight] = useState(null);
   const dropdownRef = useRef(null);
 
   useEffect(() => {
-    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
-  }, [])
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
 
   function calcHeight(el) {
     const height = el.offsetHeight;
@@ -63,7 +63,11 @@ function DropdownMenu() {
 
   function DropdownItem(props) {
     return (
-      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
         <span className="icon-button">{props.leftIcon}</span>
         {props.children}
         <span className="icon-right">{props.rightIcon}</span>
@@ -73,62 +77,78 @@ function DropdownMenu() {
 
   return (
     <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
-
       <CSSTransition
-        in={activeMenu === 'main'}
+        in={activeMenu === "main"}
         timeout={500}
         classNames="menu-primary"
         unmountOnExit
-        onEnter={calcHeight}>
+        onEnter={calcHeight}
+      >
         <div className="menu">
-          <DropdownItem>My Profile</DropdownItem>
-          <DropdownItem
-            leftIcon={<CogIcon />}
-            rightIcon={<ChevronIcon />}
-            goToMenu="settings">
-            Settings
+          <DropdownItem>Equipment</DropdownItem>
+          <DropdownItem rightIcon=">" goToMenu="evaluation">
+            Evaluation
           </DropdownItem>
-          <DropdownItem
-            leftIcon="🦧"
-            rightIcon={<ChevronIcon />}
-            goToMenu="animals">
-            Animals
+          <DropdownItem>Loss Expectancy</DropdownItem>
+          <DropdownItem>Occupancy Area</DropdownItem>
+          <DropdownItem rightIcon=">" goToMenu="recommendation">
+            Recommendation
           </DropdownItem>
-
+          <DropdownItem>Structure</DropdownItem>
         </div>
       </CSSTransition>
 
+{/* Evaluation Menu */}
       <CSSTransition
-        in={activeMenu === 'settings'}
+        in={activeMenu === "evaluation"}
         timeout={500}
         classNames="menu-secondary"
         unmountOnExit
-        onEnter={calcHeight}>
+        onEnter={calcHeight}
+      >
         <div className="menu">
           <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
-            <h2>My Tutorial</h2>
+            <h2>Back</h2>
           </DropdownItem>
-          <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
-          <DropdownItem leftIcon={<BoltIcon />}>CSS</DropdownItem>
-          <DropdownItem leftIcon={<BoltIcon />}>JavaScript</DropdownItem>
-          <DropdownItem leftIcon={<BoltIcon />}>Awesome!</DropdownItem>
+          <DropdownItem rightIcon=">" goToMenu="">
+            Construction
+          </DropdownItem>
+          <DropdownItem rightIcon=">" goToMenu="">
+            Contamination
+          </DropdownItem>
+          <DropdownItem rightIcon=">" goToMenu="">
+            Human Element
+          </DropdownItem>
+          <DropdownItem rightIcon=">" goToMenu="">
+            Natural Hazards
+          </DropdownItem>
+          <DropdownItem rightIcon=">" goToMenu="">
+            Special
+          </DropdownItem>
+          <DropdownItem rightIcon=">" goToMenu="">
+            Storages
+          </DropdownItem>
+          <DropdownItem rightIcon={<BoltIcon />} goToMenu="">
+            Other
+          </DropdownItem>
         </div>
       </CSSTransition>
-
+{/* Recommendation Menu */}
       <CSSTransition
-        in={activeMenu === 'animals'}
+        in={activeMenu === "recommendation"}
         timeout={500}
         classNames="menu-secondary"
         unmountOnExit
-        onEnter={calcHeight}>
+        onEnter={calcHeight}
+      >
         <div className="menu">
           <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
-            <h2>Animals</h2>
+            <h2>Back</h2>
           </DropdownItem>
-          <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
-          <DropdownItem leftIcon="🐸">Frog</DropdownItem>
-          <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
-          <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+          <DropdownItem>Flood</DropdownItem>
+          <DropdownItem rightIcon="🦘">Hall</DropdownItem>
+          <DropdownItem>Earth Movement</DropdownItem>
+          <DropdownItem>Wind</DropdownItem>
         </div>
       </CSSTransition>
     </div>
